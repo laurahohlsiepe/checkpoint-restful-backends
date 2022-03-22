683455f5f15377c42d36913aa219b485fe34f1d7
laurahohlsiepe <l.hohlsiepe@gmail.com>
2022-03-22 16:53:31 +0100

    names and profilphoto

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 newContacts = document.querySelector("#new-contacts");
+contactCard = document.querySelector("#contact-card");
 
 function showContacts() {
   fetch("https://dummy-apis.netlify.app/api/contact-suggestions?count=8")
     .then(function (response) {
-      response.json();
+      return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      const photo = document.createElement("img");
+      photo.src = data[0].picture;
+      newContacts.appendChild(photo);
+
+      const title = data[0].name.title;
+      const firstNames = data[0].name.first;
+      const familyNames = data[0].name.last;
+
+      const allNames = title + " " + firstNames + " " + familyNames;
+
+      contactCard.innerText = allNames;
+
+      newContacts.appendChild(contactCard);
+      contactCard.appendChild(allNames);
+
+      console.log(allNames);
     });
 }
+
+showContacts();
