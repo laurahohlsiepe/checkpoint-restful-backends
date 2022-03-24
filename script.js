5cfd3c671cea9536f5df9fc5c7ecc79270f3c06c
laurahohlsiepe <l.hohlsiepe@gmail.com>
2022-03-24 14:35:14 +0100

    design

--- script.js
+++ script.js
@@ -1,57 +1,67 @@
 const newContacts = document.querySelector("#new-contacts");
-const contactCard = document.querySelector("#contact-card");
+// const contactCard = document.querySelector("#contact-card");
 const photo = document.querySelector("#photo");
+const allCards = document.querySelector("#all-cards");
 
-function showContacts() {
-  fetch("https://dummy-apis.netlify.app/api/contact-suggestions?count=8")
-    .then(function (response) {
-      return response.json();
+for (let i = 0; i < 8; i++) {
+  getUserData();
+}
+
+function getUserData() {
+  fetch("https://dummy-apis.netlify.app/api/contact-suggestions?count=1")
+    .then(function (res) {
+      return res.json();
     })
     .then(function (data) {
-      // Profile Photo
-      photo.src = data[0].picture;
-      contactCard.appendChild(photo);
-
-      // Name
-      const friendName = document.querySelector("#friend-name");
-      const title = data[0].name.title;
-      const firstNames = data[0].name.first;
-      const familyNames = data[0].name.last;
-
-      const allNames = title + " " + firstNames + " " + familyNames;
-      friendName.innerText = allNames;
-
-      newContacts.appendChild(contactCard);
-      contactCard.appendChild(friendName);
-
-      // Profession
-      const job = document.querySelector("#job");
-      const jobOfPerson = data[0].title;
-      job.innerText = jobOfPerson;
-
-      newContacts.appendChild(contactCard);
-      contactCard.appendChild(job);
-
-      // Mutual Connections
-      const mutuals = document.querySelector("#mutuals");
-      const mutualPeople = data[0].mutualConnections;
-
-      const mutualsText = mutualPeople + " " + "mutual connections";
-      mutuals.innerText = mutualsText;
-
-      newContacts.appendChild(contactCard);
-      contactCard.appendChild(mutuals);
-
-      //background
-      const backgroundImage = data[0].backgroundImage;
-
-      contactCard.style.backgroundImage = "url(" + backgroundImage + ")";
-
-      //connect button
-      const connectBtn = document.querySelector("#connect-btn");
-      newContacts.appendChild(contactCard);
-      contactCard.appendChild(connectBtn);
+      showContacts(data);
     });
 }
 
-showContacts();
+function showContacts(data) {
+  const littleCard = document.createElement("div");
+  // Profile Photo
+  const photo = document.createElement("img");
+  photo.src = data[0].picture;
+  photo.id = "photo";
+
+  // Name
+  const friendName = document.createElement("h2");
+  friendName.id = "friend-name";
+
+  const title = data[0].name.title;
+  const firstNames = data[0].name.first;
+  const familyNames = data[0].name.last;
+
+  const allNames = title + " " + firstNames + " " + familyNames;
+  friendName.innerText = allNames;
+
+  // Profession
+  const job = document.createElement("h3");
+  const jobOfPerson = data[0].title;
+  job.innerText = jobOfPerson;
+
+  job.id = "job";
+
+  // Mutual Connections
+  const mutuals = document.createElement("p");
+  const mutualPeople = data[0].mutualConnections;
+
+  const mutualsText = mutualPeople + " " + "mutual connections";
+  mutuals.innerText = mutualsText;
+
+  //background
+  const backgroundImage = data[0].backgroundImage;
+
+  littleCard.style.backgroundImage = "url(" + backgroundImage + ")";
+
+  //connect button
+  const connectBtn = document.createElement("button");
+  connectBtn.innerText = "Connect";
+  connectBtn.id = "connect-button";
+
+  console.log(littleCard);
+  littleCard.id = "little-card";
+
+  littleCard.append(photo, friendName, job, mutuals, connectBtn);
+  allCards.appendChild(littleCard);
+}
